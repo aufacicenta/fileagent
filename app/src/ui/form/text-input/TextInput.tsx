01655a3443539319f6a476eba10b152bcf2b277a
Gustavo Ibarra <gus@aufacicenta.com>
2022-04-19 15:44:38 -0600

    feat(CreateMarketModal): category pills

--- app/src/ui/form/text-input/TextInput.tsx
+++ app/src/ui/form/text-input/TextInput.tsx
@@ -4,16 +4,27 @@ import { Field } from "react-final-form";
 import { TextInputProps } from "./TextInput.types";
 import styles from "./TextInput.module.scss";
 
-export const TextInput: React.FC<TextInputProps> = ({ className, id, type, placeholder, ...props }) => (
+export const TextInput: React.FC<TextInputProps> = ({
+  className,
+  id,
+  type,
+  placeholder,
+  name,
+  value,
+  children,
+  ...props
+}) => (
   <div className={clsx(styles["text-input"], "input-field", className)}>
     <Field
       id={id}
-      name={id}
+      name={name || id}
       component="input"
       type={type}
       className={clsx(styles["text-input"], "validate")}
       autoFocus={props.autoFocus}
       placeholder={placeholder}
+      value={value}
     />
+    {children && children}
   </div>
 );
