b618e3f22825f9dd99164fd1026cae4d0465c77a
Gustavo Ibarra <gus@aufacicenta.com>
2022-06-27 12:30:37 -0600

    chore: removes near provider contract files

--- app/src/providers/near/contract/escrow-factory.types.ts
+++
@@ -1,7 +0,0 @@
-export type EscrowFactoryValues = {
-  conditionalEscrowContractsList: string[];
-};
-
-export type EscrowFactoryMethods = {
-  get_conditional_escrow_contracts_list: () => Promise<string[]>;
-};
