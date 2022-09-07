d34562d30a970089ffc743867b692b5d2928cf2e
Gustavo Ibarra <gus@aufacicenta.com>
2022-09-07 13:28:31 -0500

    chore: creates _document component to remove link href warning

--- app/src/layouts/dashboard-layout/DashboardLayout.tsx
+++ app/src/layouts/dashboard-layout/DashboardLayout.tsx
@@ -31,7 +31,6 @@ export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) =>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600&display=swap" rel="stylesheet" />
         <meta name="description" content={t("head.og.description")} />
         <meta property="og:title" content={t("head.og.title")} />
         <meta property="og:description" content={t("head.og.description")} />
