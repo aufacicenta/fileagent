1e4eb43c63cd159e162f32f3cf4b694054825101
Gustavo Ibarra <gus@aufacicenta.com>
2022-10-20 11:47:11 -0600

    chore(CreateMarketModal): increases storage deposit

--- app/src/providers/near/getConfig.ts
+++ app/src/providers/near/getConfig.ts
@@ -11,7 +11,7 @@ const MAINNET_DAO_ACCOUNT_ID = "pulse-dao.sputnikv2.near";
 const TESTNET_DAO_URL = `https://testnet.app.astrodao.com/dao/${TESTNET_DAO_ACCOUNT_ID}/proposals?status=&category=FunctionCalls`;
 const MAINNET_DAO_URL = `https://app.astrodao.com/dao/${MAINNET_DAO_ACCOUNT_ID}/proposals?status=&category=FunctionCalls`;
 
-const TESTNET_AMM_FACTORY_ACCOUNT_ID = "amm-factory-22.aufacicenta.testnet";
+const TESTNET_AMM_FACTORY_ACCOUNT_ID = "market-factory-4.splitfund.testnet";
 const MAINNET_AMM_FACTORY_ACCOUNT_ID = "amm-factory-22.aufacicenta.near";
 
 const TESTNET_CONFIG = {
