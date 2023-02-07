02b9323c073ba23aff3b4dc33f4be3a70e2cac61
Gustavo Ibarra <gus@aufacicenta.com>
2023-02-07 15:27:12 -0600

    chore(HomeContainer): fetches latest market id

--- app/src/app/home/HomeContainer.tsx
+++ app/src/app/home/HomeContainer.tsx
@@ -1,13 +1,50 @@
+import { useEffect, useState } from "react";
+
+import { useToastContext } from "hooks/useToastContext/useToastContext";
+import { MarketFactoryContract } from "providers/near/contracts/market-factory";
 import useNearMarketContract from "providers/near/contracts/market/useNearMarketContract";
 import { GenericLoader } from "ui/generic-loader/GenericLoader";
+import { Typography } from "ui/typography/Typography";
 
 import { Home } from "./Home";
 
 export const HomeContainer = () => {
-  // @TODO fetch latest price market from market-factory contract
-  const marketId = "a1d5e2.market-factory-2.pulsemarkets.testnet";
+  const [marketId, setMarketId] = useState("");
+  const [preventLoad, setPreventLoad] = useState(true);
 
-  const { marketContractValues } = useNearMarketContract({ marketId });
+  const { marketContractValues } = useNearMarketContract({ marketId, preventLoad });
+
+  const toast = useToastContext();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const marketFactory = await MarketFactoryContract.loadFromGuestConnection();
+        const marketsList = await marketFactory.getMarketsList();
+
+        if (!marketsList) {
+          throw new Error("ERR_FAILED_TO_FETCH_MARKETS");
+        }
+
+        const latestMarketId = marketsList.pop();
+
+        if (!latestMarketId) {
+          throw new Error("ERR_MARKET_FACTORY_HAS_NO_MARKETS");
+        }
+
+        setMarketId(latestMarketId);
+        setPreventLoad(false);
+      } catch {
+        toast.trigger({
+          variant: "error",
+          withTimeout: true,
+          // @TODO i18n
+          title: "Failed to fetch recent markets",
+          children: <Typography.Text>Try refreshing the page, or check your internet connection.</Typography.Text>,
+        });
+      }
+    })();
+  }, []);
 
   if (!marketContractValues) {
     return <GenericLoader />;
