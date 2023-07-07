55ff19a96df00aa99d8f7acdbd16b07c5f90dbed
Gustavo Ibarra <gus@aufacicenta.com>
2023-07-07 14:55:49 -0600

    chore: og:image update

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;700&display=swap" rel="stylesheet" />
+        <meta property="og:image" content="/prompt-wars/reddit-ad.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/prompt-wars/reddit-ad.png" />
       </Head>
       <body>
         <Main />
