e98d96712f00037c3e4083420194959e3907e438
Gus <gus@aufacicenta.com>
2023-07-12 22:02:00 +0000

    chore: google tag manager in _app

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import { appWithTranslation } from "next-i18next";
 import { AppProps } from "next/app";
 import { setConfiguration } from "react-grid-system";
-
 import "../theme/globals.scss";
+import Script from "next/script";
 
 setConfiguration({ containerWidths: [540, 740, 960, 1280, 1540], gutterWidth: 32 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T6PR3QEEGR" />
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-T6PR3QEEGR');`,
+        }}
+      />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
