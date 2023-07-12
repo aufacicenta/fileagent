f35db8ecb21d9e96fdd2a88e7f11ec0aa8f67736
Gustavo Ibarra <gus@aufacicenta.com>
2023-07-12 15:43:34 -0600

    chore: sets Google Tag Manager

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -10,6 +11,17 @@ export default function Document() {
         <meta property="og:image" content="/prompt-wars/reddit-ad.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="/prompt-wars/reddit-ad.png" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T6PR3QEEGR" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-T6PR3QEEGR');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
