4ac1b394d1a54a52c3473a980b34d5921554afd4
Gustavo Ibarra <gus@aufacicenta.com>
2023-08-04 10:27:18 -0600

    chore: removes JSON comments

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -33,7 +33,7 @@ export const getServerSideProps = async ({ locale }: GetServerSidePropsContext) 
 
   return {
     props: {
-      ...(await serverSideTranslations(locale!, ["common", "head", "prompt-wars", "img-prompt-card"])),
+      ...(await serverSideTranslations(locale!, ["common", "head", "prompt-wars"])),
       marketId,
     },
   };
